bdc53acc5bcf1ac2590fdd8f5da5a15768c5befb
PatrickManalich <patrickmanalich@gmail.com>
2024-01-11 09:54:54 -0500

    Run on C# scripts

--- CalleCuba/Startup.cs
+++ CalleCuba/Startup.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CalleCuba
 {
@@ -30,7 +25,6 @@ namespace CalleCuba
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
