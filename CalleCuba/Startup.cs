f4356a5cd711e353ef6969ac389937c76f375d76
PatrickManalich <patrickmanalich@gmail.com>
2024-01-12 09:37:38 -0500

    Specify instantiating a InMemoryRestaurantData for the interface

--- CalleCuba/Startup.cs
+++ CalleCuba/Startup.cs
@@ -1,3 +1,4 @@
+using CalleCuba.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -19,6 +20,8 @@ namespace CalleCuba
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();   // TODO: InMemoryRestaurantData has a list field, which isn't thread safe with Singleton. It will be changed later.
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
