8c25d15b1cae8acc4f9b372163a73f9202d94efb
PatrickManalich <patrickmanalich@gmail.com>
2024-01-12 09:38:42 -0500

    Expose restaurant data in ListModel

--- CalleCuba/Pages/Restaurants/List.cshtml.cs
+++ CalleCuba/Pages/Restaurants/List.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CalleCuba.Core;
+using CalleCuba.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -11,17 +13,21 @@ namespace CalleCuba.Pages.Restaurants
     public class ListModel : PageModel
     {
         private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
 
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             _config = config;
+            _restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
             Message = _config["Message"];
+            Restaurants = _restaurantData.GetAll();
         }
     }
 }
