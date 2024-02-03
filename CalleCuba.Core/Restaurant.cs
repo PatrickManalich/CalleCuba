57f7a072b20925b57bc386fe088af74f8b74fcad
PatrickManalich <patrickmanalich@gmail.com>
2024-02-03 16:14:38 -0500

    Require form fields to be filled when editing

--- CalleCuba.Core/Restaurant.cs
+++ CalleCuba.Core/Restaurant.cs
@@ -1,10 +1,17 @@
-﻿namespace CalleCuba.Core
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalleCuba.Core
 {
     public class Restaurant
     {
         public int Id { get; set; }
+
+        [Required, StringLength(80)]
         public string Name { get; set; }
+
+        [Required, StringLength(255)]
         public string Location { get; set; }
+
         public CuisineType Cuisine { get; set; }
     }
 }
