38b730fd9df29222d6d3bfa11ad813d2857b07dc
PatrickManalich <patrickmanalich@gmail.com>
2024-02-03 13:48:08 -0500

    Properly display cuisines list

--- CalleCuba/Pages/Restaurants/Edit.cshtml.cs
+++ CalleCuba/Pages/Restaurants/Edit.cshtml.cs
@@ -1,32 +1,41 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CalleCuba.Core;
 using CalleCuba.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CalleCuba.Pages.Restaurants
 {
     public class EditModel : PageModel
     {
         private readonly IRestaurantData _restaurantData;
+        private readonly IHtmlHelper _htmlHelper;
 
         public Restaurant Restaurant { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             _restaurantData = restaurantData;
+            _htmlHelper = htmlHelper;
         }
 
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
             Restaurant = _restaurantData.GetById(restaurantId);
             if (Restaurant == null)
             {
                 return RedirectToPage("./NotFound");
             }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
             return Page();
         }
     }
