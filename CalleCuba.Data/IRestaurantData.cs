ff4900c436b987269114b5f4745da1cae6958189
PatrickManalich <patrickmanalich@gmail.com>
2024-02-03 13:44:56 -0500

    Create new methods in IRestaurantData

--- CalleCuba.Data/IRestaurantData.cs
+++ CalleCuba.Data/IRestaurantData.cs
@@ -9,6 +9,10 @@ namespace CalleCuba.Data
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
 
         Restaurant GetById(int id);
+
+        Restaurant Update(Restaurant updatedRestaurant);
+
+        int Commit();
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -27,6 +31,23 @@ namespace CalleCuba.Data
         public Restaurant GetById(int id)
         {
             return _restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = _restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
         }
 
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
